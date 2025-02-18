0c6303657a65dcedef207c1636ee87cc3020bc6d
saitewasreset <saite@buaa.edu.cn>
2025-02-18 19:49:11 +0800

    chore: add proxy for development

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,11 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+      },
+    },
+  },
 });
